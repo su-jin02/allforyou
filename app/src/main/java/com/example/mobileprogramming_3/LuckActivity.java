418d3024fcb463ba094dd88e2aff67b0422fc4d4
호연수 <yeonsooho@naver.com>
2022-10-10 15:00:51 +0900

    bottomnavi_bar

---
+++ app/src/main/java/com/example/mobileprogramming_3/LuckActivity.java
@@ -0,0 +1,17 @@
+package com.example.mobileprogramming_3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class LuckActivity extends Fragment {
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_luck, container, false);
+    }
+
+}
+
--- app/src/main/java/com/example/mobileprogramming_3/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming_3/MainActivity.java
@@ -1,14 +1,89 @@
 package com.example.mobileprogramming_3;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+
+                    case R.id.item_fragment1:
+                        getSupportFragmentManager().beginTransaction().replace(
+                                R.id.main_frame, new LuckActivity()).commit();
+                        break;
+                    case R.id.item_fragment2:
+                        getSupportFragmentManager().beginTransaction().replace(
+                                R.id.main_frame, new Test1Activity()).commit();
+                        break;
+                    case R.id.item_fragment3:
+                        getSupportFragmentManager().beginTransaction().replace(
+                                R.id.main_frame, new DiaryActivity()).commit();
+                        break;
+                    case R.id.item_fragment4:
+                        getSupportFragmentManager().beginTransaction().replace(
+                                R.id.main_frame, new SoraActivity()).commit();
+                        break;
+                    case R.id.item_fragment5:
+                        getSupportFragmentManager().beginTransaction().replace(
+                                R.id.main_frame, new MypageActivity()).commit();
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("종료").setMessage("정말로 앱을 종료하시겠습니까?");
+            builder.setPositiveButton("네", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    moveTaskToBack(true);
+                    finishAndRemoveTask();
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                }
+            });
+            builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+
+        }
+
+        return false;
+    }
+
 }
