528697c30fe205c0f7d2efbdc3227f6e035aa790
sominshin <20211358@sungshin.ac.kr>
2022-10-27 14:11:41 +0900

    start

--- app/src/main/java/com/example/mobileprogramming_3/SignupActivity.java
+++ app/src/main/java/com/example/mobileprogramming_3/SignupActivity.java
@@ -1,23 +1,42 @@
 package com.example.mobileprogramming_3;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Locale;
 
 
 public class SignupActivity extends AppCompatActivity {
-    TextView back;
-    EditText name,id,pw,pw2,email;
+    EditText name, id, pw, pw2, email, birth;
     Button pwcheck, submit;
+    Calendar myCalendar = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener myDatePicker = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, month);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,28 +44,41 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         name = findViewById(R.id.name);
-        email=findViewById(R.id.email);
-        pw=findViewById(R.id.pw1);
-        pw2=findViewById(R.id.pw2);
+        email = findViewById(R.id.email);
+        pw = findViewById(R.id.pw1);
+        pw2 = findViewById(R.id.pw2);
 
-
-        pwcheck = findViewById(R.id.pwcheck);
-        pwcheck.setOnClickListener(v -> {
-            if(pw.getText().toString().equals(pw2.getText().toString())){
-                pwcheck.setText("일치");
-            }else{
-                Toast.makeText(SignupActivity.this, "비밀번호가 다릅니다.", Toast.LENGTH_LONG).show();
+        EditText et_Date = (EditText) findViewById(R.id.birth);
+        et_Date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(SignupActivity.this, myDatePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
 
 
-        submit = findViewById(R.id.btn1);
-        submit.setOnClickListener(v -> {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
+        pwcheck = findViewById(R.id.pwcheck);
+
+        pwcheck.setOnClickListener(v -> {
+            if (pw.getText().toString().equals(pw2.getText().toString())) {
+                pwcheck.setText("일치");
+            } else {
+                Toast.makeText(SignupActivity.this, "비밀번호가 다릅니다.", Toast.LENGTH_LONG).show();
+            }
+
+
         });
-
-
-
     }
-}
+
+        private void updateLabel () {
+            String myFormat = "yyyy/MM/dd";
+            SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.KOREA);
+
+            EditText et_date = (EditText) findViewById(R.id.birth);
+            et_date.setText(sdf.format(myCalendar.getTime()));
+        }
+    }
+
+
+
+
