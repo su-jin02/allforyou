2f010cdc4d04b69e4262161a043660cd7f1311bc
JS <pjs8949124@gmail.com>
2022-11-02 00:41:52 +0900

    init git

---
+++ AndroidStudioProjects/MP/app/src/main/java/com/example/mobileprogramming_3/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.mobileprogramming_3;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Random;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    ImageView img, imageV, image;
+    ImageButton start, stop;
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = (ImageView) findViewById(R.id.img);
+        imageV = (ImageView) findViewById(R.id.imageV);
+        start = (ImageButton) findViewById(R.id.startIB);
+        stop = (ImageButton) findViewById(R.id.stopIB);
+        text = (TextView) findViewById(R.id.text);
+        image = (ImageView) findViewById(R.id.image);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new LuckActivity()).commit();
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+
+                    case R.id.item_fragment1:
+                        getSupportFragmentManager().beginTransaction().replace(
+                                R.id.main_frame, new LuckActivity()).commit();
+                        break;
+                    case R.id.item_fragment2:
+                        getSupportFragmentManager().beginTransaction().replace(
+                                R.id.main_frame, new Test1Activity()).commit();
+                        break;
+                    case R.id.item_fragment3:
+                        getSupportFragmentManager().beginTransaction().replace(
+                                R.id.main_frame, new DiaryActivity()).commit();
+                        break;
+                    case R.id.item_fragment4:
+                        getSupportFragmentManager().beginTransaction().replace(
+                                R.id.main_frame, new SoraActivity()).commit();
+                        break;
+                    case R.id.item_fragment5:
+                        getSupportFragmentManager().beginTransaction().replace(
+                                R.id.main_frame, new MypageActivity()).commit();
+                        break;
+                }
+                return true;
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("종료").setMessage("정말로 앱을 종료하시겠습니까?");
+            builder.setPositiveButton("네", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    moveTaskToBack(true);
+                    finishAndRemoveTask();
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                }
+            });
+            builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+
+        }
+
+        return false;
+    }
+
+
+
+
+}
