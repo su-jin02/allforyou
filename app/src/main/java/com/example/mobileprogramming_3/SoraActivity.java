93cd0815619e7c3781e1125e07a19358e3536ec8
JS <pjs8949124@gmail.com>
2022-11-04 12:39:57 +0900

    소라고동

--- app/src/main/java/com/example/mobileprogramming_3/SoraActivity.java
+++ app/src/main/java/com/example/mobileprogramming_3/SoraActivity.java
@@ -1,16 +1,72 @@
 package com.example.mobileprogramming_3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.FocusFinder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
+import java.util.Random;
 
 public class SoraActivity extends Fragment {
+    ImageView img,imageV,image;
+    ImageButton start, stop;
+    TextView text;
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_sora, container, false);
-    }
+        View v=inflater.inflate(R.layout.activity_sora, container, false);
 
+        ImageView img=v.findViewById(R.id.img);
+        ImageView imageV=v.findViewById(R.id.imageV);
+        ImageView image=v.findViewById(R.id.image);
+        ImageButton start=v.findViewById(R.id.startIB);
+        ImageButton stop=v.findViewById(R.id.stopIB);
+        TextView text=v.findViewById(R.id.text);
+
+        Glide.with(this).load(R.raw.yuio).into(img);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                img.setVisibility(View.VISIBLE);
+                imageV.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                img.setVisibility(View.INVISIBLE);
+                image.setVisibility(View.VISIBLE);
+                String[] randomText=getResources().getStringArray(R.array.randomText);
+                Random random=new Random();
+                int n=random.nextInt(randomText.length-1);
+                AlertDialog.Builder dlg= new AlertDialog.Builder(getActivity());
+                dlg.setTitle("소라고동");
+                dlg.setMessage(randomText[n]);
+                dlg.setPositiveButton("확인",null);
+                dlg.show();
+            }
+        });
+        return v;
+
+
+
+
+    }
 }
