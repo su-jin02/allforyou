041bf159ad3bf30913b1fcbc750d654c5408a3c6
호연수 <yeonsooho@naver.com>
2022-11-07 21:51:27 +0900

    appiconADD

--- app/src/main/java/com/example/mobileprogramming_3/MypageActivity.java
+++ app/src/main/java/com/example/mobileprogramming_3/MypageActivity.java
@@ -2,7 +2,10 @@ package com.example.mobileprogramming_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MypageActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MypageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        ImageButton mypgbackBtn = (ImageButton) findViewById(R.id.mypgbackBtn);
+
+        mypgbackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
