5e45a74efa1015eeaaaeec6d49df2398c9c0f44c
shimwoohyeon <20211362@sungshin.ac.kr>
2022-11-18 00:57:07 +0900

    commit

--- app/src/main/java/edu/sungshin/Allforyou/SignupActivity.java
+++ app/src/main/java/edu/sungshin/Allforyou/SignupActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -37,7 +39,10 @@ public class SignupActivity extends AppCompatActivity {
     private DatabaseReference mDatabaseRef;
     EditText name, id, pw, pw2, email, birth;
     Button pwcheck,btn1;
+    RadioGroup radio_group;
+    RadioButton radio_button_man, radio_button_woman, radio_button_else;
     Calendar myCalendar = Calendar.getInstance();
+
 
 
     DatePickerDialog.OnDateSetListener myDatePicker = new DatePickerDialog.OnDateSetListener() {
@@ -60,9 +65,14 @@ public class SignupActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         pw = findViewById(R.id.pw1);
         pw2 = findViewById(R.id.pw2);
+        radio_group = findViewById(R.id.radio_group);
+        radio_button_man = findViewById(R.id.radio_button_man);
+        radio_button_woman = findViewById(R.id.radio_button_woman);
+        radio_button_else = findViewById(R.id.radio_button_else);
+        EditText et_date = (EditText) findViewById(R.id.birth);
 
         mFirebaseAuth = FirebaseAuth.getInstance();
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference("Allforyou");
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("User");
 
         EditText et_Date = (EditText) findViewById(R.id.birth);
         et_Date.setOnClickListener(new View.OnClickListener() {
@@ -75,62 +85,83 @@ public class SignupActivity extends AppCompatActivity {
         btn1 = findViewById(R.id.btn1);
         pwcheck = findViewById(R.id.pwcheck);
 
-
         pwcheck.setOnClickListener(v -> {
             if (pw.getText().toString().equals(pw2.getText().toString())) {
                 pwcheck.setText("일치");
             } else {
                 Toast.makeText(SignupActivity.this, "비밀번호가 다릅니다.", Toast.LENGTH_LONG).show();
             }
-
-
         });
 
+        /*
+        radio_group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch(radio_group.getCheckedRadioButtonId()){
+                    case R.id.radio_button_man:
+                        break;
+                    case R.id.radio_button_woman:
+                        break;
+                    case R.id.radio_button_else:
+                        break;
+                }
+            }
+        });
+
+         */
 
         btn1 = findViewById(R.id.btn1);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 String stremail = email.getText().toString().trim();
                 String strpw = pw.getText().toString().trim();
-                //String strname = name.getText().toString().trim();
+                String strpw2 = pw2.getText().toString().trim();
+                String strbirth = et_date.getText().toString().trim();
+                radio_group = findViewById(R.id.radio_group);
 
-                mFirebaseAuth.createUserWithEmailAndPassword(stremail, strpw).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
+                if (strpw.equals(strpw2) && (strbirth.length() != 0) && (radio_group.getCheckedRadioButtonId() != 0)) {
+                    mFirebaseAuth.createUserWithEmailAndPassword(stremail, strpw).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
 
-                        //가입 성공시
-                        if (task.isSuccessful()) {
-                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser(); // firebaseUser에 현재 사용자 가져오기
-                            String strname = name.getText().toString().trim();
-                            UserAccount user = new UserAccount();
-                            //String email = user.getEmailId();
-                            //String uid = user.getUid();
-                            user.setIdToken(firebaseUser.getUid());
-                            //user.setUserName(strname);
-                            user.setEmailId(firebaseUser.getEmail());
-                            user.setPw(strpw);
+                            //가입 성공시
+                            if (task.isSuccessful()) {
 
-                            mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(user); // 데이터베이스에 삽입
+                                FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                                String stremail = firebaseUser.getEmail();
+                                String uid = firebaseUser.getUid();
+                                String strname = name.getText().toString().trim();
+                                String strpw = pw.getText().toString().trim();
 
-                            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-                            startActivity(intent);
-                            finish();
+                                HashMap<Object, String> hashMap = new HashMap<>();
+                                hashMap.put("uid", uid);
+                                hashMap.put("email", stremail);
+                                hashMap.put("name", strname);
+                                hashMap.put("pw", strpw);
 
-                            Toast.makeText(SignupActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
+                                mDatabaseRef.child(uid).setValue(hashMap);
 
+
+                                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                                finish();
+
+                                Toast.makeText(SignupActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
+
+                            } else {
+
+                                Toast.makeText(SignupActivity.this, "회원가입 실패", Toast.LENGTH_SHORT).show();
+                            }
                         }
-                        else{
+                    });
 
-                            Toast.makeText(SignupActivity.this, "회원가입 실패", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+                } else {
+                    Toast.makeText(SignupActivity.this, "비밀번호가 다릅니다.", Toast.LENGTH_LONG).show();
+                }
             }
         });
-
-
-
     }
 
     private void updateLabel () {
