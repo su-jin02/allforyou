87c448a3c23a73addc80e463bc52c3b157761cd7
sominshin <20211358@sungshin.ac.kr>
2022-11-21 10:55:50 +0900

    start

---
+++ app/src/main/java/edu/sungshin/Allforyou/Test3DrawResult.java
@@ -0,0 +1,26 @@
+package edu.sungshin.Allforyou;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Test3DrawResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test3result);
+
+        ImageButton btnReturn = (ImageButton) findViewById(R.id.returnbtn);
+
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
+
